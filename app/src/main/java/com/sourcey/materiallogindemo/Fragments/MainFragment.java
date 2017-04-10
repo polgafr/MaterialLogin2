493e302f06c4fdb3182e146a78dece275c10507e
Polga <polgafr@gmail.com>
2017-04-10 12:25:18 +0200

    Initial commit

---
+++ app/src/main/java/com/sourcey/materiallogindemo/Fragments/MainFragment.java
@@ -0,0 +1,19 @@
+package com.sourcey.materiallogindemo.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sourcey.materiallogindemo.R;
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main,container,false);
+    }
+}
